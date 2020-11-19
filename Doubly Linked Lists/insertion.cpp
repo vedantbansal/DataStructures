ec0ed97244137123a91ec010d6c0199e35316b26
vedantbansal <56358936+vedantbansal@users.noreply.github.com>
2020-11-19 19:51:00 +0530

    Update insertion.cpp

--- Doubly Linked Lists/insertion.cpp
+++ Doubly Linked Lists/insertion.cpp
@@ -6,9 +6,8 @@ using namespace std;
 //Declare a Node structure
 class Node{
     public:
-        Node *prev; 
         int key;
-        Node *next; 
+        Node *next;
 };
 
 Node *head = NULL; //pointer to head node of the linked list
@@ -16,7 +15,6 @@ Node *head = NULL; //pointer to head node of the linked list
 //Create the Linked list
 void createList(int data){
     Node *newNode = new Node();
-    newNode->prev = NULL;
     newNode->key = data;
     newNode->next = NULL;
 
@@ -33,46 +31,31 @@ void createList(int data){
         currentNode = currentNode->next;
     }
     currentNode->next = newNode;
-    newNode->prev = currentNode;
 }
 
 //Insert element in the list
-void insertElement(int data, int pos){
+void insert(int data, int pos){
     Node *newNode = new Node();
-    newNode->prev = NULL;
     newNode->key = data;
     newNode->next = NULL;
 
     Node *currentNode = head;
     int count = 1;
 
-    //Create head node if list is empty.
-    if (head == NULL){
-        cout<<"Sorry! List is empty.";
-    }
-
     //Insert node at first position
-    else if (pos == 1){
+    if (pos == 1){
         newNode->next = head;
-        head->prev = newNode;
         head = newNode;
+        return;
     }
 
     //Insert node in remaining list
-    else{
-        while (count<pos-1 && currentNode != NULL){
-            currentNode = currentNode->next;
-            count++;
-        }
-        newNode->next = currentNode->next;
-        currentNode->next = newNode;
-        newNode->prev = currentNode;
-
-        //if insertion is not at last position
-        if(newNode->next != NULL){
-            newNode->next->prev = newNode;
-        }
+    while (count<pos-1 && currentNode != NULL){
+        currentNode = currentNode->next;
+        count++;
     }
+    newNode->next = currentNode->next;
+    currentNode->next = newNode;
 }
 
 //Print the linked list
@@ -88,25 +71,6 @@ void printList(){
     cout<<endl;
 }
 
-//Function to print the list in backward direction.
-void backwardList(){
-    Node *currentNode = head;
-
-    //Find the address of tail node
-    while (currentNode->next != NULL){
-        currentNode = currentNode->next;
-    }
-
-    //traverse the linked list backwards
-    cout<<"Backwards List: ";
-    while (currentNode != NULL){
-        cout<<currentNode->key<<" ";
-        currentNode = currentNode->prev;
-    }
-    cout<<endl;
-
-}
-
 int main(){
     int n, x, ele, pos, choice;
     cout<<"Enter the number of elements you want to store in the list: ";
@@ -117,18 +81,20 @@ int main(){
         createList(x);
     }
 
+    //Loop to insert element in the list
     while(1){
         cout<<"Enter 1 if you want to insert an element else enter 0 to quit: ";
         cin>>choice;
         if(choice == 1){
             cout<<"Enter the element and its position: ";
             cin>>ele>>pos;
-            insertElement(ele, pos);
+            insert(ele, pos);
             printList();
         }
-        else break;
+        else{
+            break;
+        }
     }
     printList();
-    backwardList();
     return 0;
 }
